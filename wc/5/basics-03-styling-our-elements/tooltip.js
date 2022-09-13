f00ba04d58856c47680ae53b6fa67185c673f31e
Doğukan Kertlez <dogukankertlez562@hotmail.com>
2022-09-13 13:24:52 +0300

    Update

--- wc/5/basics-03-styling-our-elements/tooltip.js
+++ wc/5/basics-03-styling-our-elements/tooltip.js
@@ -2,28 +2,29 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
-    this._tooltipText = 'Some dummy tooltip text.';
+    this._tooltipText = "Some dummy tooltip text.";
   }
 
   connectedCallback() {
-    if (this.hasAttribute('text')) {
-      this._tooltipText = this.getAttribute('text');
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
     }
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = ' (?)';
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    const tooltipIcon = document.createElement("span");
+    tooltipIcon.textContent = " (?)";
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
-    this.style.position = 'relative';
+    this.style.position = "relative";
   }
 
   _showTooltip() {
-    this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = this._tooltipText;
-    this._tooltipContainer.style.backgroundColor = 'black';
-    this._tooltipContainer.style.color = 'white';
-    this._tooltipContainer.style.position = 'absolute';
-    this._tooltipContainer.style.zIndex = '10';
+    this._tooltipContainer = document.createElement("div");
+    this.tooltipContainer.this._tooltipContainer.textContent =
+      this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = "black";
+    this._tooltipContainer.style.color = "white";
+    this._tooltipContainer.style.position = "absolute";
+    this._tooltipContainer.style.zIndex = "10";
     this.appendChild(this._tooltipContainer);
   }
 
@@ -32,4 +33,4 @@ class Tooltip extends HTMLElement {
   }
 }
 
-customElements.define('uc-tooltip', Tooltip);
+customElements.define("uc-tooltip", Tooltip);
